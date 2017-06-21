66c0d7f4ca032873256115d4029053fa98944822
Gregory Kan <kan.gregory@gmail.com>
2017-06-21 15:13:18 +1200

    added redux logger

--- app/helpers/initFormStorybook.js
+++ app/helpers/initFormStorybook.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { default as logger } from 'redux-logger'
 import { reducer as formReducer } from 'redux-form'
 
 const reducer = combineReducers({
   form: formReducer
 })
 
-const store = createStore(reducer)
+const enhancer = applyMiddleware(
+  logger
+)
+
+const store = createStore(reducer, undefined, enhancer)
 
 export default () => story => {
   return (
