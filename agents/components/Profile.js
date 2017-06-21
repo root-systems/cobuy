0cfa562b251a637933ba8dacb7a0054e90b0d796
Gregory Kan <kan.gregory@gmail.com>
2017-06-21 17:01:34 +1200

    added avatar field into profile

--- agents/components/Profile.js
+++ agents/components/Profile.js
@@ -6,12 +6,18 @@ import { flow } from 'lodash'
 import { TextField } from 'redux-form-material-ui'
 
 import styles from '../styles/Profile'
+import AvatarField from '../../app/components/AvatarField'
 
 function Profile (props) {
   const { styles, profile, handleSubmit } = props
   const { name, description } = profile
   return (
     <form className={styles.container}>
+      <Field
+        name='avatar'
+        label='Avatar'
+        component={AvatarField}
+      />
       <Field
         name='name'
         floatingLabelText='Name'
@@ -39,5 +45,12 @@ Profile.defaultProps = {
 
 export default flow(
   connectFela(styles),
-  connectForm({ form: 'profile '})
+  connectForm({
+    form: 'profile',
+    initialValues: {
+      avatar: 'http://dinosaur.is/images/mikey-small.jpg',
+      name: 'classic nixon',
+      description: "it's classic nixon"
+    }
+  })
 )(Profile)
