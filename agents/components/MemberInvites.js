9c8dbfd2afff43a455a06152fd5b2698d179ff60
Gregory Kan <kan.gregory@gmail.com>
2017-06-22 17:48:05 +1200

    refactored to use functions instead of react component class

--- agents/components/MemberInvites.js
+++ agents/components/MemberInvites.js
@@ -8,49 +8,47 @@ import { TextField } from 'redux-form-material-ui'
 import styles from '../styles/MemberInvites'
 import Button from '../../app/components/Button'
 
-class MemberInvites extends React.Component {
-  renderMembers ({ fields, meta: { error, submitFailed } }) {
-    return (
-      <div>
-        <Button type='button' onClick={() => fields.push({})}>Add Member</Button>
-        {submitFailed && error && <span>{error}</span>}
-        {fields.map((member, index) => (
-          <div key={index}>
-            <Field
-              name={`${member}.name`}
-              floatingLabelText='Name'
-              component={TextField}
-            />
-            <Field
-              name={`${member}.email`}
-              floatingLabelText='Email'
-              component={TextField}
-            />
-            <Field
-              name={`${member}.role`}
-              floatingLabelText='Role'
-              component={TextField}
-            />
-            <Button type='button' onClick={() => fields.remove(index)}>Remove Member</Button>
-          </div>
-        ))}
-      </div>
-    )
-  }
+function renderMembers ({ fields, meta: { error, submitFailed } }) {
+  return (
+    <div>
+      <Button type='button' onClick={() => fields.push({})}>Add Member</Button>
+      {submitFailed && error && <span>{error}</span>}
+      {fields.map((member, index) => (
+        <div key={index}>
+          <Field
+            name={`${member}.name`}
+            floatingLabelText='Name'
+            component={TextField}
+          />
+          <Field
+            name={`${member}.email`}
+            floatingLabelText='Email'
+            component={TextField}
+          />
+          <Field
+            name={`${member}.role`}
+            floatingLabelText='Role'
+            component={TextField}
+          />
+          <Button type='button' onClick={() => fields.remove(index)}>Remove Member</Button>
+        </div>
+      ))}
+    </div>
+  )
+}
 
-  render () {
-    return (
-      <form className={styles.container}>
-        <Field
-          name='groupName'
-          type='text'
-          component={TextField}
-          floatingLabelText='Group Name'
-        />
-        <FieldArray name="members" component={this.renderMembers} />
-      </form>
-    )
-  }
+function MemberInvites (props) {
+  return (
+    <form className={styles.container}>
+      <Field
+        name='groupName'
+        type='text'
+        component={TextField}
+        floatingLabelText='Group Name'
+      />
+      <FieldArray name='members' component={renderMembers} />
+    </form>
+  )
 }
 
 export default flow(
