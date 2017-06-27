fa088b89674eb1705180cd250573d9ff084a9f81
Gregory Kan <kan.gregory@gmail.com>
2017-06-27 17:51:02 +1200

    trimmed story for offering

--- offerings/stories/AddOffering.js
+++ offerings/stories/AddOffering.js
@@ -1,18 +1,9 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { reduxForm, Field } from 'redux-form'
 
 import AddOffering from '../components/AddOffering'
 
-const alice = {
-  profile: {
-    name: 'Alice',
-    description: 'a cool cat',
-    avatar: 'http://dinosaur.is/images/mikey-small.jpg'
-  }
-}
 storiesOf('agents.AddOffering', module)
   .add('basic', () => (
-    <AddOffering agent={alice} />
+    <AddOffering />
   ))
