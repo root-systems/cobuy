210334a5aee1dcbadd6d04040bf40e44e8c223e4
Gregory Kan <kan.gregory@gmail.com>
2017-07-04 15:04:04 +1200

    added new theme object

---
+++ app/themes/base.js
@@ -0,0 +1,44 @@
+import {
+  teal500,
+  teal600,
+  teal700,
+  white,
+  darkBlack,
+  deepPurple500,
+  deepPurple600,
+  deepPurple700,
+  grey300,
+  fullBlack
+} from 'material-ui/styles/colors'
+
+export default {
+  breakpoints: {
+    mobile: '13.6em',
+    mobileWide: '30.4em',
+    tablet: '47.2em',
+    desktop: '64em',
+    desktopLarge: '97.6em'
+  },
+  fontSize: '15px',
+  fontFamily: 'Roboto, sans-serif',
+  weightThin: 100,
+  weightLight: 300,
+  weightRegular: 400,
+  weightMedium: 500,
+  weightBold: 700,
+  em: '1em',
+  rem: '1rem',
+  borderRadius: '2px',
+  zIndexMax: 1000,
+  primary1Color: teal500,
+  primary2Color: teal600,
+  primary3Color: teal700,
+  accent1Color: deepPurple500,
+  accent2Color: deepPurple600,
+  accent3Color: deepPurple700,
+  textColor: white,
+  alternateTextColor: darkBlack,
+  canvasColor: white,
+  borderColor: grey300,
+  shadowColor: fullBlack
+}
