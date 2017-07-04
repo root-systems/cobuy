fec8d4baa4ebc19abb1aef06e6f53bb4559dec9a
Gregory Kan <kan.gregory@gmail.com>
2017-07-04 12:55:23 +1200

    feat(style): added a bunch of colors to basic theme

--- app/themes/basic.js
+++ app/themes/basic.js
@@ -1,9 +1,30 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import {cyan500} from 'material-ui/styles/colors'
+import {
+  teal500,
+  teal600,
+  teal700,
+  white,
+  darkblack,
+  deeppurple500,
+  deeppurple600,
+  deeppurple700,
+  grey300,
+  fullBlack
+} from 'material-ui/styles/colors'
 
 export default getMuiTheme({
   fontFamily: 'Roboto, sans-serif',
   palette: {
-    textColor: cyan500
+    primary1Color: teal500,
+    primary2Color: teal600,
+    primary3Color: teal700,
+    accent1Color: deeppurple500,
+    accent2Color: deeppurple600,
+    accent3Color: deeppurple700,
+    textColor: white,
+    alternateTextColor: darkblack,
+    canvasColor: white,
+    borderColor: grey300,
+    shadowColor: fullBlack
   }
 })
