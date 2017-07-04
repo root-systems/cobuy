645be01a43936ba1f80061cfed09d0abc6dc4a63
Gregory Kan <kan.gregory@gmail.com>
2017-07-04 12:38:19 +1200

    feat(style): scaffolded basic mui theme to test

--- .storybook/helpers/initMuiStorybook.js
+++ .storybook/helpers/initMuiStorybook.js
@@ -1,12 +1,13 @@
 /* global document */
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import basicTheme from '../../app/themes/basic'
 
 export default () =>
   story => {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={basicTheme}>
         {story()}
       </MuiThemeProvider>
-    );
+    )
   }
---
+++ app/themes/basic.js
@@ -0,0 +1,9 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {cyan500} from 'material-ui/styles/colors'
+
+export default getMuiTheme({
+  fontFamily: 'Roboto, sans-serif',
+  palette: {
+    textColor: cyan500
+  }
+})
