3765a879c1ad741e3c37c0aa34ec31fc02d60229
Sarah Rogers <srogersnz@gmail.com>
2017-07-06 15:28:57 +1200

    min, des + max relate to each other sensibly

--- ordering/components/MemberIntentField.js
+++ ordering/components/MemberIntentField.js
@@ -27,6 +27,9 @@ function PreIntent (props) {
   return (
     <Slider
       onChange={handleChange}
+      min={0}
+      max={10}
+      step={1}
     />
   )
 
@@ -39,28 +42,50 @@ function PreIntent (props) {
   }
 }
 
+/*
+  {renderSlider({ name: 'minimum', min: 0, max: value.desired })}
+  {renderSlider({ name: 'desired', min: value.minimum, max: value.maximum })}
+  {renderSlider({ name: 'maximum', max: 10, min: value.desired })}
+*/
+
 function Intent (props) {
   const { onChange, value } = props
 
   return (
     <div>
-      {renderSlider('minimum')}
-      {renderSlider('desired')}
-      {renderSlider('maximum')}
+      {renderSlider({ name: 'minimum' })}
+      {renderSlider({ name: 'desired' })}
+      {renderSlider({ name: 'maximum' })}
     </div>
   )
 
-  function renderSlider (key) {
+  function renderSlider (options) {
+    const { name } = options
     return (
       <Slider
-        onChange={handleChange(key)}
-        value={value[key]}
+        onChange={handleChange(name)}
+        value={value[name]}
+        min={0}
+        max={10}
+        step={1}
       />
     )
   }
 
   function handleChange (key) {
     return (ev, newValue) => {
+      if (key == 'minimum') {
+        // minimum can't be more than desired
+        newValue = Math.min(newValue, value.desired)
+      } else if (key == 'desired') {
+        // desired can't be less than minimum
+        newValue = Math.max(newValue, value.minimum)
+        // desired can't be more than maximum
+        newValue = Math.min(newValue, value.maximum)
+      } else if (key == 'maximum') {
+        // maximum can't be less than desired
+        newValue = Math.max(newValue, value.desired)
+      }
       onChange(merge(value, { [key]: newValue }))
     }
   }
