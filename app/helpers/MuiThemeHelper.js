ab961bd835aa1d7295f88b9d438d3d1c0b7d2836
Gregory Kan <kan.gregory@gmail.com>
2017-07-11 14:26:11 +1200

    feat(theme): restructured theme based on styled-system's opinion

--- app/helpers/MuiThemeHelper.js
+++ app/helpers/MuiThemeHelper.js
@@ -4,17 +4,17 @@ export default function (theme) {
   return getMuiTheme({
     fontFamily: theme.fontFamily,
     palette: {
-      primary1Color: theme.colorPrimary1,
-      primary2Color: theme.colorPrimary2,
-      primary3Color: theme.colorPrimary3,
-      accent1Color: theme.colorAccent1,
-      accent2Color: theme.colorAccent2,
-      accent3Color: theme.colorAccent3,
-      textColor: theme.colorText,
-      alternateTextColor: theme.colorAlternateText,
-      canvasColor: theme.colorCanvas,
-      borderColor: theme.colorBorder,
-      shadowColor: theme.colorShadow
+      primary1Color: theme.colors.teal500,
+      primary2Color: theme.colors.teal600,
+      primary3Color: theme.colors.teal700,
+      accent1Color: theme.colors.deepPurple500,
+      accent2Color: theme.colors.deepPurple600,
+      accent3Color: theme.colors.deepPurple700,
+      textColor: theme.colors.darkBlack,
+      alternateTextColor: theme.colors.white,
+      canvasColor: theme.colors.white,
+      borderColor: theme.colors.grey300,
+      shadowColor: theme.colors.fullBlack
     }
   })
 }
