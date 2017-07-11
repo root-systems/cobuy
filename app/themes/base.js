3340ba55e96b5fe3fe1497138ef7ff5c41721010
Gregory Kan <kan.gregory@gmail.com>
2017-07-12 10:49:25 +1200

    feat(theme): use semantic keys for colours

--- app/themes/base.js
+++ app/themes/base.js
@@ -33,25 +33,29 @@ export default {
     12, 16, 18, 24, 36, 72
   ],
   colors: {
-    teal500,
-    teal600,
-    teal700,
-    deepPurple500,
-    deepPurple600,
-    deepPurple700,
-    grey50,
-    grey100,
-    grey200,
-    grey300,
-    grey400,
-    grey500,
-    grey600,
-    grey700,
-    grey800,
-    grey900,
-    darkBlack,
-    fullBlack,
-    white
+    primary1: teal500,
+    primary2: teal600,
+    primary3: teal700,
+    accent1: deepPurple500,
+    accent2: deepPurple600,
+    accent3: deepPurple700,
+    greys: [
+      grey50,
+      grey100,
+      grey200,
+      grey300,
+      grey400,
+      grey500,
+      grey600,
+      grey700,
+      grey800,
+      grey900
+    ],
+    text: darkBlack,
+    alternateText: white,
+    canvas: white,
+    border: grey300,
+    shadow: fullBlack
   },
   fontWeights: {
     thin: 100,
@@ -61,5 +65,9 @@ export default {
     bold: 700,
     black: 900
   },
-  fontFamily: 'Roboto, sans-serif'
+  em: '1em',
+  rem: '1rem',
+  fontFamily: 'Roboto, sans-serif',
+  borderRadius: '2px',
+  zIndexMax: 100
 }
