2a0928587ec43c97f8a3ce5b3837721ded44e2f1
Gregory Kan <kan.gregory@gmail.com>
2017-07-12 13:36:14 +1200

    chore(offerings): added basic styling to offerings component

--- supply/components/Offerings.js
+++ supply/components/Offerings.js
@@ -5,6 +5,7 @@ import { Field, reduxForm as connectForm, FormSection, FieldArray } from 'redux-
 import { pipe } from 'ramda'
 import { SelectField, TextField, Toggle } from 'redux-form-material-ui'
 import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import { FormattedMessage } from '../../lib/Intl'
 import styles from '../styles/Offerings'
@@ -21,95 +22,103 @@ class Offering extends React.Component {
   render () {
     const { fields, offering, index, isChild, styles } = this.props
     return (
-      <div key={index}>
-        {
-          isChild
-          ? null
-          : <Button type='button' title='Remove Offering' onClick={() => fields.remove(index)}>
+      <div key={index} className={styles.fieldContainer}>
+        <div className={styles.rowContainer}>
+          <Field
+            name={ isChild ? 'quantity' : `${offering}.quantity` }
+            floatingLabelText={
               <FormattedMessage
-                id='supply.removeOffering'
-                className={styles.buttonText}
-              />
-            </Button>
-        }
-        <Field
-          name={ isChild ? 'quantity' : `${offering}.quantity` }
-          floatingLabelText={
-            <FormattedMessage
-              id='supply.quantity'
-              className={styles.labelText}
-            />
-          }
-          component={TextField}
-        />
-        <Field
-          name={ isChild ? 'unit' : `${offering}.unit`}
-          floatingLabelText={
-            <FormattedMessage
-              id='supply.unit'
-              className={styles.labelText}
-            />
-          }
-          component={SelectField}
-        >
-          <MenuItem
-            value='kg'
-            primaryText={
-              <FormattedMessage
-                id='supply.kg'
+                id='supply.quantity'
                 className={styles.labelText}
               />
             }
+            component={TextField}
           />
-          <MenuItem
-            value='litres'
-            primaryText={
+          <Field
+            name={ isChild ? 'unit' : `${offering}.unit`}
+            floatingLabelText={
               <FormattedMessage
-                id='supply.litres'
+                id='supply.unit'
                 className={styles.labelText}
               />
             }
-          />
-          <MenuItem
-            value='ea'
-            primaryText={
+            component={SelectField}
+          >
+            <MenuItem
+              value='kg'
+              primaryText={
+                <FormattedMessage
+                  id='supply.kg'
+                  className={styles.labelText}
+                />
+              }
+            />
+            <MenuItem
+              value='litres'
+              primaryText={
+                <FormattedMessage
+                  id='supply.litres'
+                  className={styles.labelText}
+                />
+              }
+            />
+            <MenuItem
+              value='ea'
+              primaryText={
+                <FormattedMessage
+                  id='supply.each'
+                  className={styles.labelText}
+                />
+              }
+            />
+          </Field>
+          <Field
+            name={ isChild ? 'resource' : `${offering}.resource`}
+            floatingLabelText={
               <FormattedMessage
-                id='supply.each'
+                id='supply.resource'
                 className={styles.labelText}
               />
             }
+            component={TextField}
           />
-        </Field>
-        <Field
-          name={ isChild ? 'resource' : `${offering}.resource`}
-          floatingLabelText={
-            <FormattedMessage
-              id='supply.resource'
-              className={styles.labelText}
+          <div className={styles.switchContainer}>
+            <Field
+              name={ isChild ? 'hasNested' : `${offering}.hasNested`}
+              component={Toggle}
+              label={
+                <FormattedMessage
+                  id='supply.contains'
+                  className={styles.labelText}
+                />
+              }
+              labelPosition='left'
+              defaultToggled={false}
+              value={this.state.hasChildOffering}
+              onChange={(e, val) => { this.setState({ hasChildOffering: val }) }}
+              elementStyle={{ padding: 0, margin: 0 }}
             />
-          }
-          component={TextField}
-        />
-        <Field
-          name={ isChild ? 'hasNested' : `${offering}.hasNested`}
-          component={Toggle}
-          label={
-            <FormattedMessage
-              id='supply.unit'
-              className={styles.labelText}
-            />
-          }
-          labelPosition='left'
-          defaultToggled={false}
-          value={this.state.hasChildOffering}
-          onChange={(e, val) => { this.setState({ hasChildOffering: val }) }}
-        />
+          </div>
+        </div>
         {
           this.state.hasChildOffering
           ? <FormSection name={ isChild ? 'offering' : `${offering}.offering`}>
-              <Offering isChild={true} key={index} />
+              <Offering isChild={true} key={index} styles={styles} />
             </FormSection>
           : null
+        }
+        {
+          isChild
+          ? null
+          : <div className={styles.rowContainer}><div className={styles.removeOfferingButtonContainer}>
+              <RaisedButton className={styles.button} type='button' title='Remove Offering' onClick={() => fields.remove(index)}>
+                <FormattedMessage
+                  id='supply.removeOffering'
+                  className={styles.buttonText}
+                />
+              </RaisedButton>
+            </div>
+            </div>
         }
       </div>
     )
@@ -120,16 +129,18 @@ class OfferingList extends React.Component {
   render () {
     const { fields, styles } = this.props
     return (
-      <div>
+      <div className={styles.fieldsContainer}>
         {fields.map((offering, index) =>
           <Offering key={index} fields={fields} offering={offering} index={index} isChild={false} styles={styles} />
         )}
-        <Button type='button' onClick={() => fields.push({})}>
-          <FormattedMessage
-            id='supply.addOffering'
-            className={styles.buttonText}
-          />
-        </Button>
+        <div className={styles.addOfferingButtonContainer}>
+          <RaisedButton className={styles.button} type='button' onClick={() => fields.push({})}>
+            <FormattedMessage
+              id='supply.addOffering'
+              className={styles.buttonText}
+            />
+          </RaisedButton>
+        </div>
       </div>
     )
   }
@@ -139,7 +150,13 @@ class Offerings extends React.Component {
   render () {
     const { handleSubmit, styles } = this.props
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.container}>
+        <p className={styles.intro}>
+          <FormattedMessage
+            id='supply.offerings'
+            className={styles.labelText}
+          />
+        </p>
         <FieldArray name='offerings' component={OfferingList} styles={styles} />
       </form>
     )
--- supply/styles/Offerings.js
+++ supply/styles/Offerings.js
@@ -1,7 +1,50 @@
 export default {
-  container: () => ({}),
+  container: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  intro: () => ({
+    textAlign: 'center',
+    fontSize: '2rem'
+  }),
   labelText: () => ({
     textTransform: 'capitalize'
+  }),
+  fieldsContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  fieldContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  offeringContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  rowContainer: () => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    width: '100%'
+  }),
+  switchContainer: () => ({
+    width: '12rem',
+    paddingBottom: '0.5rem'
+  }),
+  addOfferingButtonContainer: () => ({
+    paddingTop: '2rem'
+  }),
+  removeOfferingButtonContainer: () => ({
+    display: 'flex',
+    paddingTop: '1rem'
+  }),
+  button: () => ({
+    width: '12rem'
   }),
   buttonText: () => ({
     textTransform: 'capitalize'
