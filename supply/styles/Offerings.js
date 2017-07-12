a242c0a6664693e0799d99c4aaaa3447d194098d
Gregory Kan <gregorykan@users.noreply.github.com>
2017-07-12 17:30:46 +1200

    chore/style offerings component (#90)
    * chore(offerings): added basic styling to offerings component
    
    * chore(offerings): added flex wrap

--- supply/styles/Offerings.js
+++ supply/styles/Offerings.js
@@ -1,7 +1,46 @@
 export default {
-  container: () => ({}),
+  container: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  intro: () => ({
+    textAlign: 'center',
+    fontSize: '2rem'
+  }),
   labelText: () => ({
     textTransform: 'capitalize'
+  }),
+  fieldsContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  fieldContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }),
+  rowContainer: () => ({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    flexWrap: 'wrap',
+    width: '100%'
+  }),
+  switchContainer: () => ({
+    width: '12rem',
+    paddingBottom: '0.5rem'
+  }),
+  addOfferingButtonContainer: () => ({
+    paddingTop: '2rem'
+  }),
+  removeOfferingButtonContainer: () => ({
+    display: 'flex',
+    paddingTop: '1rem'
+  }),
+  button: () => ({
+    width: '12rem'
   }),
   buttonText: () => ({
     textTransform: 'capitalize'
