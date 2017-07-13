a77251b9122e2901ecd04d5467bbe80df67d610e
Gregory Kan <kan.gregory@gmail.com>
2017-07-13 15:40:15 +1200

    feat(navigation): added drawer with some placeholder items

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
 
-function AppBarWithIcon () {
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+
+  handleToggle = () => this.setState({ open: !this.state.open })
+
+//   render() {
+//     return (
+//       <div>
+//         <RaisedButton
+//           label="Toggle Drawer"
+//           onTouchTap={this.handleToggle}
+//         />
+//         <Drawer open={this.state.open}>
+//           <MenuItem>Menu Item</MenuItem>
+//           <MenuItem>Menu Item 2</MenuItem>
+//         </Drawer>
+//       </div>
+//     );
+//   }
+// }
+
+render () {
   return (
-    <AppBar
-      title="Cobuy"
-      iconElementRight={<i className="fa fa-bars" aria-hidden="true"></i>}
-    />
+    <div>
+      <AppBar
+        title="Cobuy"
+        iconElementRight={<i className="fa fa-bars" aria-hidden="true" onClick={this.handleToggle}></i>}
+      />
+      <Drawer open={this.state.open}>
+        <MenuItem>Menu Item</MenuItem>
+        <MenuItem>Menu Item 2</MenuItem>
+      </Drawer>
+    </div>
   )
 }
 
-export default AppBarWithIcon
+// export default Navigation
