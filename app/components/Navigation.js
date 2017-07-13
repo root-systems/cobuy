f62c2ec1fc1d29da92918026305080fc15a643a1
Gregory Kan <kan.gregory@gmail.com>
2017-07-13 20:32:44 +1200

    chore(purity): use recompose to handle state

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,80 +1,73 @@
 import React from 'react'
 import { connect as connectFela } from 'react-fela'
-import { pipe } from 'ramda'
+import { not } from 'ramda'
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import Divider from 'material-ui/Divider'
+import { withState, withHandlers, compose } from 'recompose'
 
 import styles from '../styles/Navigation'
 import { FormattedMessage } from '../../lib/Intl'
 
-class Navigation extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      drawerOpen: false
-    }
-  }
+function Navigation (props) {
+  const { styles, isDrawerOpen, toggleDrawer } = props
 
-  handleDrawerToggle = () => {
-    this.setState({ drawerOpen: !this.state.drawerOpen })
-  }
-
-  render () {
-    const { styles } = this.props
-    return (
-      <div>
-        <AppBar
-          title={
-            <FormattedMessage
-              id='app.name'
-              className={styles.labelText}
-            />
+  return (
+    <div>
+      <AppBar
+        title={
+          <FormattedMessage
+            id='app.name'
+            className={styles.labelText}
+          />
+        }
+        onLeftIconButtonTouchTap={toggleDrawer}
+      />
+      <Drawer open={isDrawerOpen}>
+        <MenuItem
+          leftIcon={
+            <i className="fa fa-bars" aria-hidden="true"/>
           }
-          onLeftIconButtonTouchTap={this.handleDrawerToggle}
-        />
-        <Drawer open={this.state.drawerOpen}>
-          <MenuItem
-            leftIcon={
-              <i className="fa fa-bars" aria-hidden="true"/>
-            }
-            onTouchTap={this.handleDrawerToggle}
-          >
-            <FormattedMessage
-              id='app.closeMenu'
-              className={styles.labelText}
-            />
-          </MenuItem>
-          <Divider />
-          <MenuItem
-            leftIcon={
-              <i className="fa fa-tachometer" aria-hidden="true" />
-            }
-          >
-            <FormattedMessage
-              id='app.dashboard'
-              className={styles.labelText}
-            />
-          </MenuItem>
-          <Divider />
-          <MenuItem
-            leftIcon={
-              <i className="fa fa-sign-out" aria-hidden="true" />
-            }
-          >
-            <FormattedMessage
-              id='app.logOut'
-              className={styles.labelText}
-            />
-          </MenuItem>
-          <Divider />
-        </Drawer>
-      </div>
-    )
-  }
+          onTouchTap={toggleDrawer}
+        >
+          <FormattedMessage
+            id='app.closeMenu'
+            className={styles.labelText}
+          />
+        </MenuItem>
+        <Divider />
+        <MenuItem
+          leftIcon={
+            <i className="fa fa-tachometer" aria-hidden="true" />
+          }
+        >
+          <FormattedMessage
+            id='app.dashboard'
+            className={styles.labelText}
+          />
+        </MenuItem>
+        <Divider />
+        <MenuItem
+          leftIcon={
+            <i className="fa fa-sign-out" aria-hidden="true" />
+          }
+        >
+          <FormattedMessage
+            id='app.logOut'
+            className={styles.labelText}
+          />
+        </MenuItem>
+        <Divider />
+      </Drawer>
+    </div>
+  )
 }
 
-export default pipe(
-  connectFela(styles)
+export default compose(
+  connectFela(styles),
+  withState('isDrawerOpen', 'setDrawerOpen', false),
+  withHandlers({
+    toggleDrawer: ({ setDrawerOpen }) => () => setDrawerOpen(not)
+  })
 )(Navigation)
