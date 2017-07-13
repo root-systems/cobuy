f06decd70a0cd1a79f90e9b4e36e6eb87f511636
Michael Williams <michael.williams@enspiral.com>
2017-07-13 18:12:13 +1200

    feat(db): add task migration

---
+++ db/migrations/20170713153018_add-tasks.js
@@ -0,0 +1,21 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.createTableIfNotExists('taskPlans', function (table) {
+      table.increments('id')
+      table.string('taskRecipeId').notNullable()
+      table.string('assignee').notNullable()
+    }),
+    knex.schema.createTableIfNotExists('taskWorks', function (table) {
+      table.increments('id')
+      table.integer('taskPlanId').references('taskPlans.id').notNullable()
+      table.integer('workerAgentId').references('agents.id').notNullable()
+    })
+  ])
+}
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('taskPlans'),
+    knex.schema.dropTableIfExists('taskWorks')
+  ])
+}
