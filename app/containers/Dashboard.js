f7327ceadb086921d64f7b0ad429f416d314f73b
Gregory Kan <kan.gregory@gmail.com>
2017-07-14 19:36:13 +1200

    feat(dashboard): updated container to handle both plan and work actions/queries

--- app/containers/Dashboard.js
+++ app/containers/Dashboard.js
@@ -3,14 +3,24 @@ import { connect } from 'feathers-action-react'
 import Dashboard from '../components/Dashboard'
 
 import { actions as taskPlanActions } from '../../tasks/dux/plans'
+import { actions as taskWorkActions } from '../../works/dux/plans'
 
 import { getDashboardProps } from '../getters'
 
 export default connect({
   selector: getDashboardProps,
-  actions: { taskPlans: taskPlanActions },
-  query: {
-    service: 'taskPlans',
-    params: {}
-  }
+  actions: {
+    taskPlans: taskPlanActions,
+    taskWorks: taskWorkActions
+  },
+  query: [
+    {
+      service: 'taskPlans',
+      params: {}
+    },
+    {
+      service: 'taskWorks',
+      params: {}
+    }
+  ]
 })(Dashboard)
