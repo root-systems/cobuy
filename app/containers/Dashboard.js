0dd3fd73d44407e89e9c4b36818602e24a766141
Gregory Kan <kan.gregory@gmail.com>
2017-07-14 17:42:38 +1200

    feat(dashboard): added container for dashboard

---
+++ app/containers/Dashboard.js
@@ -0,0 +1,16 @@
+import { connect } from 'feathers-action-react'
+
+import Dashboard from '../components/Dashboard'
+
+import { actions as taskPlanActions } from '../../tasks/plans'
+
+import { getDashboardProps } from '../getters'
+
+export default connect({
+  selector: getDashboardProps,
+  actions: { taskPlans: taskPlanActions },
+  query: {
+    service: 'taskPlans',
+    params: {}
+  }
+})(Dashboard)
