43a62722cb3c3b714c6885a7540c90ea3e516a7f
Gregory Kan <kan.gregory@gmail.com>
2017-07-19 11:46:43 +1200

    chore(tests): added some basic tests for tasks getters

---
+++ tasks/getters/getters.test.js
@@ -0,0 +1,56 @@
+import test from 'ava'
+
+import getRawTaskPlans from './getRawTaskPlans'
+import getRawTaskRecipes from './getRawTaskRecipes'
+import getRawTaskWorks from './getRawTaskWorks'
+
+import { finishPrereqs } from '../data/recipes'
+
+const agent = {
+  id: 1,
+  profile: {
+    name: 'Sam'
+  }
+}
+
+const taskPlan = {
+  id: 1,
+  agent,
+  taskRecipeId: 'finishPrereqs'
+}
+
+const taskWork = {
+  id: 1,
+  taskPlanId: 1,
+  agent
+}
+
+const mockTaskPlans = {
+  1: taskPlan
+}
+
+const mockTaskRecipes = {
+  finishPrereqs: finishPrereqs
+}
+
+const mockTaskWorks = {
+  1: taskWork
+}
+
+const mockState = {
+  taskPlans: mockTaskPlans,
+  taskRecipes: mockTaskRecipes,
+  taskWorks: mockTaskWorks
+}
+
+test('getRawTaskPlans', t => {
+  t.deepEqual(getRawTaskPlans(mockState), mockTaskPlans)
+})
+
+test('getRawTaskRecipes', t => {
+  t.deepEqual(getRawTaskRecipes(mockState), mockTaskRecipes)
+})
+
+test('getRawTaskWorks', t => {
+  t.deepEqual(getRawTaskWorks(mockState), mockTaskWorks)
+})
