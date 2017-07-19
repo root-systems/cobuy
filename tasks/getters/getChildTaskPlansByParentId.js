957fdf37068fe4cec1f020f60c46490fbc03655d
Gregory Kan <kan.gregory@gmail.com>
2017-07-19 12:57:10 +1200

    chore(tests): reverted getter to piped form

--- tasks/getters/getChildTaskPlansByParentId.js
+++ tasks/getters/getChildTaskPlansByParentId.js
@@ -3,20 +3,12 @@ import { pipe, values, groupBy, prop } from 'ramda'
 
 import getEnhancedTaskPlans from './getEnhancedTaskPlans'
 
-// const getChildTaskPlansByParentId = createSelector(
-//   getEnhancedTaskPlans,
-//   pipe(
-//     values,
-//     groupBy(prop('parentTaskPlanId'))
-//   )
-// )
-
 const getChildTaskPlansByParentId = createSelector(
   getEnhancedTaskPlans,
-  (enhancedTaskPlans) => {
-    const byParentTaskPlanId = groupBy(prop('parentTaskPlanId'))
-    return byParentTaskPlanId(values(enhancedTaskPlans))
-  }
+  pipe(
+    values,
+    groupBy(prop('parentTaskPlanId'))
+  )
 )
 
 export default getChildTaskPlansByParentId
