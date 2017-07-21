fb452df586ad8f6586d60e4323fa8d12efb77048
Michael Williams <michael.williams@enspiral.com>
2017-07-21 14:33:25 +1200

    Revert "skip optimistic set"
    This reverts commit fb1e34d60dbfe4e9de3f01415fd6fab08cae7a65.

--- tasks/util/createTaskPlan.js
+++ tasks/util/createTaskPlan.js
@@ -13,13 +13,14 @@ export default function createTaskPlan (action$, cid, options) {
     parentTaskPlanId
   }
 
+  console.log('taskPlan', parentTaskPlan)
+
   var nextActions$ = Rx.Observable.of(taskPlans.create(cid, parentTaskPlan))
 
   if (childTaskRecipes.length > 0) {
-    const parentTaskPlanSet$ = action$.ofType(taskPlans.set.type).filter(onlyCid).skip(1).take(1)
+    const parentTaskPlanSet$ = action$.ofType(taskPlans.set.type).filter(onlyCid).take(1)
 
-    nextActions$ = Rx.Observable.concat(
-      nextActions$,
+    nextActions$ = nextActions$.concat(
       parentTaskPlanSet$.mergeMap(action => {
         return createChildTaskPlans(action.payload.data.id)
       })
