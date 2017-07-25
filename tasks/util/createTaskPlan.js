9d86557f4ce6272deca760a042359e944c6011d3
Gregory Kan <kan.gregory@gmail.com>
2017-07-25 14:58:20 +1200

    feat(tasks): reverted to older task plan creation code

--- tasks/util/createTaskPlan.js
+++ tasks/util/createTaskPlan.js
@@ -1,49 +1,23 @@
-import Rx from 'rxjs'
-import Cid from 'incremental-id'
+import { unnest } from 'ramda'
 
-import { actions as taskPlans } from '../dux/plans'
-
-export default function createTaskPlan (action$, cid, options) {
-  const { assignee, taskRecipe, parentTaskPlanId } = options
+export default function createTaskPlan (options) {
+  const { assignee, taskRecipe } = options
   const { childTaskRecipes = [] } = taskRecipe
 
   const parentTaskPlan = {
     assignee,
-    taskRecipeId: taskRecipe.id,
-    parentTaskPlanId
+    taskRecipeId: taskRecipe.id
   }
 
-  console.log('taskPlan', parentTaskPlan)
+  const childTaskPlans = childTaskRecipes.map(childTaskRecipe => {
+    return createTaskPlan({
+      assignee,
+      taskRecipe: childTaskRecipe
+    })
+  })
 
-  var nextActions$ = Rx.Observable.of(taskPlans.create(cid, parentTaskPlan))
-
-  if (childTaskRecipes.length > 0) {
-    const parentTaskPlanSet$ = action$.ofType(taskPlans.set.type).filter(onlyCid).take(1)
-
-    nextActions$ = nextActions$.concat(
-      parentTaskPlanSet$.mergeMap(action => {
-        return createChildTaskPlans(action.payload.data.id)
-      })
-    )
-  }
-
-  return nextActions$
-
-  function createChildTaskPlans (parentTaskPlanId) {
-    // (mw) need a sub-cid otherwise maybe epic will mess up?
-    return Rx.Observable.merge(
-      ...childTaskRecipes.map(childTaskRecipe => {
-        const subCid = `${cid}/${Cid()}`
-        return createTaskPlan(action$, subCid, {
-          assignee,
-          taskRecipe: childTaskRecipe,
-          parentTaskPlanId
-        })
-      })
-    )
-  }
-
-  function onlyCid (action) {
-    return action.meta.cid === cid
-  }
+  return unnest([
+    parentTaskPlan,
+    ...childTaskPlans
+  ])
 }
