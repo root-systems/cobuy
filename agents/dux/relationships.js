05c11da2173a2daa86b8e463ccfcdafc8eac0e16
Gregory Kan <kan.gregory@gmail.com>
2017-07-28 12:21:02 +1200

    chore(refactor): this stuff is going to be in dogstack-agents

--- agents/dux/relationships.js
+++
@@ -1,7 +0,0 @@
-import createModule from 'feathers-action'
-
-const module = createModule('agentRelationships')
-
-export const actions = module.actions
-export const updater = module.updater
-export const epic = module.epic
