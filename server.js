5cdee0cd16665263187ad097a7769e1a3b79eddb
Gregory Kan <kan.gregory@gmail.com>
2017-07-28 12:21:31 +1200

    chore(refactor): this stuff is going to be in dogstack-agents

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const services = [
   require('dogstack-agents/service'),
   require('./tasks/services/plans'),
   require('./tasks/services/works'),
-  require('./ordering/services/orders'),
-  require('./agents/services/relationships')
+  require('./ordering/services/orders')
 ]
 
 export default {
