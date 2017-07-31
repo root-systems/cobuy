9e396e6a760f030bdf038012ca75163d1dc8afe5
Sarah Rogers <srogersnz@gmail.com>
2017-07-31 16:27:35 +1200

    (style): add Lobster2 font to style.js, create fonts object and update font scale in base.js

--- app/themes/base.js
+++ app/themes/base.js
@@ -41,15 +41,35 @@ export default {
     '16rem',
   ],
   fontSizes: [
-    '0.875rem',
-    '1rem',
-    '1.25rem',
-    '1.5rem',
-    '2.25rem',
-    '3rem',
-    '5rem',
-    '6rem'
+    '0.8rem', // [0] 10pt
+    '0.95rem', // [1] 11pt
+    '1rem', // [2] 12pt
+    '1.2rem', // [3] 14pt
+    '1.4rem', // [4] 16pt
+    '1.5rem', // [5] 18pt
+    '1.7rem', // [6] 21pt
+    '2rem', // [7] 24pt
+    '3rem', // [8] 36pt
+    '4rem', // [9] 48pt
+    '5rem', // [10] 60pt
+    '6rem', // [11] 72pt
+    '8rem' // [12] 96pt
+    // og scale:
+    // '0.875rem',
+    // '1rem',
+    // '1.25rem',
+    // '1.5rem',
+    // '2.25rem',
+    // '3rem',
+    // '5rem',
+    // '6rem',
+    // '8rem',
+    // '10rem'
   ],
+  fonts: {
+    primary: 'Roboto, sans-serif',
+    logo: '"Lobster Two", cursive'
+  },
   colors: {
     primary1: teal500,
     primary2: teal600,
@@ -85,7 +105,6 @@ export default {
   },
   em: '1em',
   rem: '1rem',
-  fontFamily: 'Roboto, sans-serif',
   borderRadius: '2px',
   zIndexMax: 1000
 }
