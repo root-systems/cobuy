04db9768f7439bb1280b0e65c73a992edc3bae83
Michael Smith <michael@msmith.nz>
2017-08-01 14:20:44 +0100

    chore(story): add schema-faker to Profile story

--- agents/stories/Profile.js
+++ agents/stories/Profile.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { reduxForm, Field } from 'redux-form'
+import jsf from 'json-schema-faker'
 
+import schema from '../schemas/profile'
 import Profile from '../components/Profile'
 
-const alice = {
-  profile: {
-    name: 'Alice',
-    description: 'a cool cat',
-    avatar: 'http://dinosaur.is/images/mikey-small.jpg'
-  }
+const fakeAgent = {
+  profile: jsf(schema)
 }
+
 storiesOf('agents.Profile', module)
   .add('basic', () => (
-    <Profile agent={alice} />
+      <Profile agent={fakeAgent} />
   ))
