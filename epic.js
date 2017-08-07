31acb51b42896fc05d526be7fdee85a49d675f3a
Michael Williams <michael.williams@enspiral.com>
2017-08-07 17:12:35 +1000

    chore(epic): yeah nah epic is part of `dogstack-agents` meow

--- epic.js
+++ epic.js
@@ -4,11 +4,9 @@ import { epic as agents } from 'dogstack-agents'
 import { epic as taskPlans } from './tasks/dux/plans'
 import { epic as taskWorks } from './tasks/dux/works'
 import { epic as orders } from './ordering/dux/orders'
-import { epic as agentRelationships } from './agents/dux/relationships'
 
 export default combineEpics(
   agents,
-  agentRelationships,
   orders,
   taskPlans,
   taskWorks
