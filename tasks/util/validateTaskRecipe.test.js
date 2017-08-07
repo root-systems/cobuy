18fdac07297298e12495eba88db24d5537759ef0
Michael Smith <michael@msmith.nz>
2017-08-07 14:27:48 +0200

    chore(tests): add test for failed task recipe validation

--- tasks/util/validateTaskRecipe.test.js
+++ tasks/util/validateTaskRecipe.test.js
@@ -6,20 +6,38 @@ const ajv = require('ajv')
 const { validateSchema } = require('feathers-hooks-common')
 const schema = require('../schemas/taskRecipe')
 
-test('create simple recipe', (t) => {
-  const hookBefore = {
+let hookBefore
+
+test.beforeEach(t => {
+  hookBefore = {
     type: 'before',
     method: 'create',
     params: { provider: 'rest' },
     data: taskRecipes.setupSupplier
   }
+})
+
+test('works with simple recipe', (t) => {
   try {
     validateSchema(schema, ajv)(hookBefore)
   } catch (err) {
     if (err.errors) {
       console.log('validation errors: ', err.errors)
     }
-    t.fail('creation failed unexpectedly')
+    t.fail('validation failed unexpectedly')
   }
-  t.pass('recipe was created')
+  t.pass('recipe was validated')
+})
+
+test('fails with incorrect id', (t) => {
+  hookBefore.data.id = 'incorrectId'
+
+  try {
+    validateSchema(schema, ajv)(hookBefore)
+    t.fail('validation passed unexpectedly')
+  } catch (err) {
+    t.deepEqual(err.errors, [
+      "'id' should be equal to one of the allowed values"
+    ])
+  }
 })
