fa023e1cdff0589a1567eb3d140cc7a12faef696
Sarah Rogers <srogersnz@gmail.com>
2017-08-14 12:21:04 +1200

    feat(landing): add sign in and register buttons

--- app/components/home.js
+++ app/components/home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect as connectFela } from 'react-fela'
 import { compose } from 'recompose'
 import { FormattedMessage } from '../../lib/Intl'
+import { Link } from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
 
 import styles from '../styles/Home'
 
@@ -19,6 +21,24 @@ function Home (props) {
         </p>
       </div>
       <div className={styles.bodyContainer}>
+        <div className={styles.buttonsContainer}>
+          <Link to={"/sign-in"}>
+            <RaisedButton primary={true}>
+              <FormattedMessage
+                id='agents.signIn'
+                className={styles.buttonText}
+              />
+            </RaisedButton>
+          </Link>
+          <Link to={"/register"}>
+            <RaisedButton primary={true}>
+              <FormattedMessage
+                id='agents.register'
+                className={styles.buttonText}
+              />
+            </RaisedButton>
+          </Link>
+        </div>
         <p className={styles.bodyText}>
           Cobuy is an app that makes buying groups easy to start, maintain, and grow. Using the collective buying power of a group, we can buy food in bulk directly from wholesalers. By cutting out retailers, we effectively eliminate retail food waste, save money and have access to a wider range of better quality products.
         </p>
