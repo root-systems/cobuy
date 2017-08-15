53b311bfae869b1bda7274787861efc4585f2c03
Michael Williams <michael.williams@enspiral.com>
2017-08-15 23:31:03 +1200

    chore(redux-form-validators): upgrade to patched version 1.1.0 with `main` set to `lib/index.js`

--- agents/components/Register.js
+++ agents/components/Register.js
@@ -7,7 +7,7 @@ import { TextField } from 'redux-form-material-ui'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 import FontIcon from 'material-ui/FontIcon'
-import { required, email, length, confirmation } from 'redux-form-validators'
+import { required, email, length, confirmation } from '@root-systems/redux-form-validators'
 
 import { FormattedMessage } from '../../lib/Intl'
 import styles from '../styles/Register'
