f060bf65712ebc6e199d2bf6c643303afd67a7df
dev <dev@Daniels-MacBook-Pro.local>
2017-08-18 14:40:17 +1200

    migration for order agent type ids

---
+++ db/migrations/20170818142530_add-agent-type-ids-to-orders.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table('orders', function(table){
+    table.renameColumn('agentId', 'consumerAgentId')
+    table.integer('supplierAgentId').references('agents.id')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('orders', function(table){
+    table.renameColumn('consumerAgentId', 'agentId')
+    table.dropColumn('supplierAgentId')
+  })
+};
