4b1dbe8656441ec84cef258684fc4cfb65b66374
dev <dev@Daniels-MacBook-Pro.local>
2017-08-18 12:03:58 +1200

    copied group profile tasks as basic supply tasks

--- tasks/components/SetupSupplierTask.js
+++ tasks/components/SetupSupplierTask.js
@@ -1,6 +1,45 @@
-import React from 'react'
+import h from 'react-hyperscript'
+import { isNil, merge, isEmpty } from 'ramda'
+
+import TaskStepper from './TaskStepper'
+import Profile from '../../agents/components/Profile'
+import MemberInvites from '../../agents/components/MemberInvites'
+
+const rolesToRelationships = (roles = {}) => {
+  var relationships = [
+    { relationshipType: 'member' }
+  ]
+  if (roles.admin) {
+    relationships.push({
+      relationshipType: 'admin'
+    })
+  }
+  return relationships
+}
 
 export default (props) => {
-  const { taskPlan } = props
-  return <div>Setup supplier yo!</div>
+  const { taskPlan, actions } = props
+  if (isNil(taskPlan)) return null
+  const { params: { contextAgent } } = taskPlan
+  if (isNil(contextAgent)) return null
+
+  const { profile, members } = contextAgent
+
+  console.log('members', members)
+
+  const steps = [
+    {
+      id: 'tasks.steps.groupProfile',
+      content: h(Profile, {
+        initialValues: profile,
+        updateProfile: (nextProfile) => {
+          actions.profiles.update(profile.id, merge(nextProfile, { agentId: contextAgent.id }))
+        }
+      })
+    },
+  ]
+
+  return h(TaskStepper, {
+    steps
+  })
 }
