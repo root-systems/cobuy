b53e85fc959fb9d989047cd37ddb7c592488b7a7
Sarah Rogers <srogersnz@gmail.com>
2017-08-22 11:37:52 +1200

    feat(profile): move agents service file up a folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const services = [
   require('./tasks/services/plans'),
   require('./tasks/services/works'),
   require('./ordering/services/orders'),
-  require('./agents/services/hooks/agents')
+  require('./agents/services/agents')
 ]
 
 export default {
