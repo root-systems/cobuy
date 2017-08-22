28fc4d73960b1ca3c7a944cb99bb4d608bcbc3da
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-08-22 14:49:38 +1200

    hook on createAgent that creates and sends a test signup email

--- agents/services/agents.js
+++ agents/services/agents.js
@@ -13,6 +13,18 @@ const hooks = {
             taskRecipeId: 'createProfile',
             params: {}
           })
+          .then(() => {
+            return hook.app.service('credentials').get(hook.result.id)
+          })
+          .then((cred) => {
+            // TODO: IK: looks like the email isn't getting attached to the credential correctly, investigate dogstack-agents
+            return hook.app.service('mailer').create({
+              from: 'hello@cobuy.nz',
+              to: cred.email || 'no@email.com',
+              subject: 'Test Cobuy SignUp Email',
+              html: 'test email'
+            })
+          })
           .then(() => hook)
         }
       }
