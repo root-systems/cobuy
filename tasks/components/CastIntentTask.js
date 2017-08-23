7c848bd905297b5c357f40690e098ca690e515b4
Sarah Rogers <srogersnz@gmail.com>
2017-08-23 15:09:38 +1200

    feat(tasks): CastIntentTask renders hard coded intent casting form

--- tasks/components/CastIntentTask.js
+++ tasks/components/CastIntentTask.js
@@ -1,20 +1,73 @@
 import h from 'react-hyperscript'
 import { isNil } from 'ramda'
+import { reduxForm as connectForm, Field } from 'redux-form'
+import { compose } from 'recompose'
 
-import styles from '../styles/CastIntentTask'
+import MemberIntentField from '../../ordering/components/MemberIntentField'
 
-export default (props) => {
+// import styles from '../styles/CastIntentTask'
+
+const avoOffering = {
+  resourceType: {
+    name: 'crate of avocados',
+    items: [
+      {
+        resourceType: {
+          name: 'avocado'
+        },
+        quantity: {
+          step: '1',
+          value: '24',
+          unit: 'each'
+        }
+      },
+      {
+        resourceType: {
+          name: 'crate'
+        },
+        quantity: {
+          value: '1',
+          unit: 'each'
+        }
+      }
+    ]
+  },
+  priceSpecifications: [
+    {
+      minimum: '0',
+      price: '100',
+      currency: 'NZD'
+    }
+  ]
+}
+
+const IntentForm = props => {
+  const { handleSubmit } = props
+  return (
+    h('form', {
+      onSubmit: () => { console.log('lunch') }
+    }, [
+      h(Field, {
+        name: 'intent',
+        component: MemberIntentField,
+        offering: avoOffering
+      })
+    ])
+  )
+}
+
+const CastIntentTask = compose(
+  connectForm({ form: 'memberIntent' }),
+  // connectStyles(styles)
+)(props => {
   const { actions, currentAgent } = props
 
   if(isNil(currentAgent)) {
     return null
   }
 
-  return (
-    h('div', {
-      className: styles.container
-    },
-      'hello i am a string'
-    )
-  )
-}
+  return h(IntentForm, props)
+})
+
+export default CastIntentTask
+
