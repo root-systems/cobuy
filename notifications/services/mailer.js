dee74cbd7593797b90f28f21400a33ce6b961916
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-08-28 10:34:05 +1200

    cleanup

--- notifications/services/mailer.js
+++ notifications/services/mailer.js
@@ -1,9 +1,7 @@
 const feathersMailer = require('feathers-mailer')
-const nodemailer = require('nodemailer')
 
 module.exports = function () {
   const app = this
-  const db = app.get('db')
 
   const name = 'mailer'
   const config = app.get(name)
