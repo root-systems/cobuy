87bcf78e1689c7d585b70e4246a9a44efdd27d3a
Juliet <to.julietc@gmail.com>
2017-09-06 11:27:27 +1200

    added feathers services

---
+++ supply/services/products.js
@@ -0,0 +1,18 @@
+const feathersKnex = require('feathers-knex')
+
+module.exports = function () {
+  const app = this
+  const db = app.get('db')
+
+  const name = 'products'
+  const options = { Model: db, name }
+
+  app.use(name, feathersKnex(options))
+  app.service(name).hooks(hooks)
+}
+
+const hooks = {
+  before: {},
+  after: {},
+  error: {}
+}
