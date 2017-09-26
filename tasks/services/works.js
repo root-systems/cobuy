14bec01f6814f5225235992fd9ec020a98305d80
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-09-26 19:49:06 +1300

    towards a hook on taskWorks for creating a castOrderIntent taskPlan when order is ready to go

--- tasks/services/works.js
+++ tasks/services/works.js
@@ -1,4 +1,5 @@
 const feathersKnex = require('feathers-knex')
+const { iff } = require('feathers-hooks-common')
 
 module.exports = function () {
   const app = this
@@ -13,6 +14,30 @@ module.exports = function () {
 
 const hooks = {
   before: {},
-  after: {},
+  after: {
+    create: [
+      iff(completeOrderSetupTaskWork, createCastOrderIntentTaskPlan)
+    ]
+  },
   error: {}
+}
+
+// TODO: IK:
+/*
+- check if the taskWork's taskRecipeId was completeOrderSetupWithPrereqs or completeOrderSetup
+- if so, then create a castOrderIntentTaskPlan for every member of the group selected for the created order
+  - we determine what the group was by looking at hook.data.taskPlanId which references the taskPlan, then grab the consumerAgentId of that taskPlan which references the group for the order
+
+- check admin of order is being assigned properly?
+*/
+
+function completeOrderSetupTaskWork (hook) {
+  console.log(hook.data)
+  return hook.data.taskRecipeId === 'completeOrderSetupWithPrereqs'
+  || hook.data.taskRecipeId === 'completeOrderSetup'
+}
+
+function createCastOrderIntentTaskPlan (hook) {
+
+  return hook
 }
