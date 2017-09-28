694d2e09435d0d7f162eceef6b3bfd730b9ac263
Sarah Rogers <srogersnz@gmail.com>
2017-09-28 17:06:49 +1300

    style(resources): flesh out resource story data before sarah's computer dies

--- resources/stories/ResourceTypeEditor.js
+++ resources/stories/ResourceTypeEditor.js
@@ -5,13 +5,25 @@ import { action } from '@storybook/addon-actions'
 import ResourceTypeEditor from '../components/ResourceTypeEditor'
 
 const resourceTypes = {
-  1: { id: 1, name: 'egg' },
-  2: { id: 2, name: 'carton' }
+  1: {
+    id: 101,
+    name: 'crayons',
+    description: 'these are crayons. look at all the pretty colours! they are made of beeswax. you could probably eat them and not die.',
+    image: 'http://www.mercurius-australia.com/site/images/1250623.jpg'
+  },
+  2: {
+    id: 102,
+    name: 'coloured pencils',
+    description: 'these are pencils that are coloured',
+    image: 'http://thewoodenwagon.com/Merchant2/graphics/00000001/ACST85090912-2.jpg'
+  }
 }
 
 const nestedResourceType = {
-  id: 3,
+  id: 103,
   name: 'carton of eggs',
+  description: "you gotta love 'em!",
+  image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/eggs_slideshow/getty_photo_of_eggs_in_carton.jpg',
   items: [
     {
       quantity: {
