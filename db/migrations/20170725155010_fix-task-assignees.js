9e8248d0aad08cf561f732bee416f060222fed17
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-10-04 15:19:39 +1300

    fix taskPlans assigneeId relationship

--- db/migrations/20170725155010_fix-task-assignees.js
+++ db/migrations/20170725155010_fix-task-assignees.js
@@ -1,7 +1,7 @@
 exports.up = function (knex, Promise) {
   return knex.schema.table('taskPlans', function (table) {
     table.dropColumn('assignee')
-    table.integer('assigneeId').references('assignee.id')
+    table.integer('assigneeId').references('agents.id')
   })
 }
 
