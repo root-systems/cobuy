285c988e43c9780471a8c938e7a749767f27757d
Michael Smith <michael@msmith.nz>
2017-10-09 16:01:02 +0100

    Add basic invoice looking order summary

--- ordering/components/OrderSummary.js
+++ ordering/components/OrderSummary.js
@@ -2,17 +2,27 @@ import { compose } from 'recompose'
 import { connect as connectFela } from 'react-fela'
 import h from 'react-hyperscript'
 import Paper from 'material-ui/Paper'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import { find, propEq } from 'ramda'
 import { FormattedMessage } from '../../lib/Intl'
 import ProductList from './ProductList'
 
 // import styles from '../styles/ListViewProduct'
 
-function OrderSummary (props) {
+function OrderSummary ({ orderSummary }) {
   // const { styles, order } = props
-/*
-  Need to know:
-    Product summary info - are we storign oders somewhere already?
-*/
+  /*
+    Need to know:
+      Product summary info - are we storign oders somewhere already?
+  */
+  console.log('this is: ', find(propEq('id', orderSummary.orderPlans[0].intent.product.priceSpecId))(orderSummary.orderPlans[0].intent.product.priceSpecs))
   return (
     h('div', {}, [
       h(Paper, {
@@ -21,7 +31,29 @@ function OrderSummary (props) {
         [
           h('h2', {}, 'Order Summary')
         ]),
-      h(ProductList, { products: props.order.products }, [])
+      h(Table, {}, [
+        h(TableHeader, { displaySelectAll: false }, [
+          h(TableRow, {}, [
+            h(TableHeaderColumn, {}, 'Product Name'),
+            h(TableHeaderColumn, {}, 'Name'),
+            h(TableHeaderColumn, {}, 'Quanity'),
+            h(TableHeaderColumn, {}, 'Item Price'),
+            h(TableHeaderColumn, {}, 'Total')
+          ])
+        ]),
+        h(TableBody, { displayRowCheckbox: false }, [
+          orderSummary.orderPlans.map((plan) => {
+            const price = find(propEq('id', plan.intent.priceSpecId))(plan.intent.product.priceSpecs).price
+            return h(TableRow, {}, [
+              h(TableRowColumn, {}, plan.intent.product.resourceType.name),
+              h(TableRowColumn, {}, plan.intent.agent.profile.name),
+              h(TableRowColumn, {}, plan.quantity),
+              h(TableRowColumn, {}, find(propEq('id', plan.intent.priceSpecId))(plan.intent.product.priceSpecs).price),
+              h(TableRowColumn, {}, (plan.quantity * price))
+            ])
+          })
+        ])
+      ])
     ])
   )
 }
--- ordering/stories/OrderSummary.js
+++ ordering/stories/OrderSummary.js
@@ -4,19 +4,72 @@ import products from './mock/products'
 
 import OrderSummary from '../components/OrderSummary'
 
-// https://github.com/root-systems/cobuy/wiki/Models#offering
-const supplier = {
-  name: 'Central Nuts'
+/*
+  OrderPlan
+  Like an invoice - the whole group's order that's going to be executed and sent to the supplier
+
+      agent
+      order
+      offering
+      resource(Type)
+      quantity
+----------------------
+      Order
+
+      products
+      intents
+      plans
+      name
+      description
+
+-----------------------
+
+OrderIntent
+
+  unique per agent, order, resource(Type)
+
+  An individual's desired order
+
+      agent
+      order
+      resource(Type)
+      desiredQuantity
+      minimumQuantity
+      maximumQuantity
+
+*/
+
+const agent = {
+  profile: {
+    name: 'Ella Banks'
+  }
 }
 
-const order = {
-  dateCompleted: '',
-  totalPrice: '',
-  supplier,
-  products
+// desired order, optimistic thinking
+const intents = [{
+  // agent that made the order
+  agent,
+  desiredQuantity: 1,
+  minimumQuantity: 1,
+  maximumQuantity: 2,
+  productId: products[0].id,
+  product: products[0],
+  priceSpecId: 456
+}]
+
+// What has actually happened
+const orderPlan = {
+  quantity: 1,
+  intent: intents[0]
+}
+
+const orderSummary = {
+  orderPlans: [
+    orderPlan
+  ]
 }
 
 storiesOf('ordering.orderSummary', module)
   .add('default', () => (
-    <OrderSummary order={order} />
+    <OrderSummary orderSummary={orderSummary} />
   ))
