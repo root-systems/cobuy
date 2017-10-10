71572ac721647d4de49809ba6ee3f6d26e202113
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-10-10 16:23:03 +1300

    create a token for an invited group member, send it to em in an email with a link - removed datetime checking for now as it's erroring

--- agents/services/agents.js
+++ agents/services/agents.js
@@ -8,21 +8,33 @@ const hooks = {
     create: [
       (hook) => {
         if (hook.result.type === 'person') {
+          const agentId = hook.result.id
+
           return hook.app.service('taskPlans').create({
-            assigneeId: hook.result.id,
+            assigneeId: agentId,
             taskRecipeId: 'createProfile',
             params: {}
           })
           .then(() => {
-            return hook.app.service('credentials').find({ query: { agentId: hook.result.id } })
+            return Promise.all([
+              hook.app.service('credentials').find({ query: { agentId } }),
+              hook.app.service('tokens').create({
+                agentId,
+                service: 'credentials',
+                method: 'patch',
+                createdAt: new Date().toString()
+              })
+            ])
           })
-          .then(([cred]) => {
+          .then(([cred, token]) => {
             // TODO: IK: looks like the email isn't getting attached to the credential correctly, investigate dogstack-agents
             return hook.app.service('mailer').create({
               from: 'hello@cobuy.nz',
               to: cred.email || 'no@email.com',
-              subject: 'Test Cobuy SignUp Email',
-              html: 'test email'
+              subject: `You're invited to join Cobuy!`,
+              html: `
+                Hi -name-. You've been invited by -admin- to join -group- on Cobuy! Click <a href=http://localhost:3000/invited/${token.jwt}>here</a> to set your password and start co-buying!
+              `
             })
           })
           .then(() => hook)
