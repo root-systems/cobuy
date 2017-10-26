c26fbb5fd5859a7a3959690713feb0615b49fcf6
Sarah Rogers <srogersnz@gmail.com>
2017-10-26 18:16:23 +1300

    add mock group info to order summary storybook

--- ordering/stories/mock/order.js
+++ ordering/stories/mock/order.js
@@ -5,11 +5,62 @@ const agents = [{
     id: 121,
     name: 'Ella Banks'
   }
-},
-{
+}, {
   profile: {
     id: 334,
     name: 'Kath Uru'
+  }
+}]
+
+const groups = [{
+  profile: {
+    id: 903,
+    name: "Ruru Kāhui Ako",
+    description: "Ruru Kāhui Ako",
+    contactInfo: {
+      contactPerson: 'Tina Turner',
+      deliveryAddress: {
+        addressLine1: '13 The Street',
+        city: 'The City',
+        postcode: '1234'
+      },
+      billingAddress: {
+        addressLine1: 'PO Box 789',
+        addressLine2: 'City Mail Centre',
+        city: 'The City',
+        postcode: '1234'
+      },
+      email: 'tina@ruru.nz',
+      phone: {
+        main: '06-1234567',
+        mobile: '022-3344556'
+      }
+    }
+  }
+}, {
+  profile: {
+    id: 904,
+    name: "School Supplies R'Us",
+    description: "If you're yearning for some learning, we'll set your mind churning",
+    contactInfo: {
+      contactPerson: 'Juliet Carpenter',
+      deliveryAddress: {
+        addressLine1: '11 The Street',
+        city: 'Auckland',
+        postcode: '9876'
+      },
+      billingAddress: {
+        addressLine1: 'PO Box 567',
+        addressLine2: 'Auckland Mail Centre',
+        city: 'Auckland',
+        postcode: '9876'
+      },
+      email: 'tina@schoolsuppliesrus.nz',
+      phone: {
+        main: '03-9876543',
+        mobile: '022-9988776'
+      }
+    }
   }
 }]
 
