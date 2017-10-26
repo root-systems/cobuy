ba2ea1512fe61a06c2ac639423c0ebe2b92f9bdd
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-10-26 14:48:53 +1300

    fix castIntentTask using the actual order data rather than params from taskPlan

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const services = [
   require('./ordering/services/orders'),
   require('./credentials/services/credentials'),
   require('./ordering/services/orderIntents'),
-  require('./agents/services/agents'),
+  require('./credentials/services/credentials'),
   require('./notifications/services/mailer'),
   require('./supply/services/products'),
   require('./supply/services/priceSpecs'),
