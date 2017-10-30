7402981f4597730f67291bdf5827b26239a15e93
Michael Smith <michael@msmith.nz>
2017-10-30 16:00:56 +0000

    add a closeOrderTask
    WIP:
    -  to be triggered when an order is created
    - container and component copied and paster from `StartOrderTask`, still needs to be changed

---
+++ tasks/getters/getCloseOrderTaskProps.js
@@ -0,0 +1,20 @@
+import { createStructuredSelector } from 'reselect'
+import getOrders from '../../ordering/getters/getOrders'
+import getCurrentAgent from 'dogstack-agents/agents/getters/getCurrentAgent'
+import getCurrentAgentGroupIds from '../../agents/getters/getCurrentAgentGroupIds'
+import getCurrentAgentGroupSupplierIds from '../../agents/getters/getCurrentAgentGroupSupplierIds'
+import getCurrentAgentGroupMemberIds from '../../agents/getters/getCurrentAgentGroupMemberIds'
+import getCurrentAgentGroupProfiles from '../../agents/getters/getCurrentAgentGroupProfiles'
+import getCurrentAgentGroupSupplierProfiles from '../../agents/getters/getCurrentAgentGroupSupplierProfiles'
+import getCurrentAgentGroupMemberProfiles from '../../agents/getters/getCurrentAgentGroupMemberProfiles'
+
+export default createStructuredSelector({
+  currentAgent: getCurrentAgent,
+  orders: getOrders,
+  currentAgentGroupIds: getCurrentAgentGroupIds,
+  currentAgentGroupSupplierIds: getCurrentAgentGroupSupplierIds,
+  currentAgentGroupMemberIds: getCurrentAgentGroupMemberIds,
+  currentAgentGroupProfiles: getCurrentAgentGroupProfiles,
+  currentAgentGroupSupplierProfiles: getCurrentAgentGroupSupplierProfiles,
+  currentAgentGroupMemberProfiles: getCurrentAgentGroupMemberProfiles
+})
