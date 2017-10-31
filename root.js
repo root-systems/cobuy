b83088f9f672ad61853807e87f7543dccdccd6ca
Michael Williams <michael.williams@enspiral.com>
2017-10-31 14:48:31 +1300

    what are these doing here?

--- root.js
+++ root.js
@@ -1,14 +1,3 @@
-import { addLocaleData } from 'react-intl'
-import en from 'react-intl/locale-data/en'
-
-addLocaleData([...en])
-
-const messagesByLocale = {
-  'en': require('./app/locales/en'),
-  'en-US': require('./app/locales/en-us')
-}
-
 export default {
-  appNode: '#app',
-  messagesByLocale
+  appNode: '#app'
 }
