462c797ad66d3a98e01092e2575e1cc103751e05
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-09 11:06:22 +1300

    clean up closeOrder task imports / queries

--- tasks/components/CloseOrderTask.js
+++ tasks/components/CloseOrderTask.js
@@ -1,22 +1,7 @@
 import h from 'react-hyperscript'
-import { isNil, merge } from 'ramda'
-
-import SelectAgentForOrder from '../../agents/components/SelectAgentForOrder'
-
-// need to pass down the choices to each instance of the selection component
 
 export default (props) => {
-  const { taskPlan, actions, orders, currentAgentGroupProfiles, currentAgentGroupSupplierProfiles, currentAgentGroupMemberProfiles } = props
-  if (isNil(taskPlan)) return null
-  const { params: { orderId } } = taskPlan
-  if (isNil(orderId)) return null
-
-  const currentOrder = orders[orderId]
-
-  // console.log('closing order for: ', currentOrder)
   return h('div', {}, [
     h('h2', {}, 'Closing order for ...')
-  ]
-
-  )
+  ])
 }
--- tasks/getters/getCloseOrderTaskProps.js
+++ tasks/getters/getCloseOrderTaskProps.js
@@ -1,20 +1,6 @@
 import { createStructuredSelector } from 'reselect'
-import getOrders from '../../ordering/getters/getOrders'
 import getCurrentAgent from 'dogstack-agents/agents/getters/getCurrentAgent'
-import getCurrentAgentGroupIds from '../../agents/getters/getCurrentAgentGroupIds'
-import getCurrentAgentGroupSupplierIds from '../../agents/getters/getCurrentAgentGroupSupplierIds'
-import getCurrentAgentGroupMemberIds from '../../agents/getters/getCurrentAgentGroupMemberIds'
-import getCurrentAgentGroupProfiles from '../../agents/getters/getCurrentAgentGroupProfiles'
-import getCurrentAgentGroupSupplierProfiles from '../../agents/getters/getCurrentAgentGroupSupplierProfiles'
-import getCurrentAgentGroupMemberProfiles from '../../agents/getters/getCurrentAgentGroupMemberProfiles'
 
 export default createStructuredSelector({
-  currentAgent: getCurrentAgent,
-  orders: getOrders,
-  currentAgentGroupIds: getCurrentAgentGroupIds,
-  currentAgentGroupSupplierIds: getCurrentAgentGroupSupplierIds,
-  currentAgentGroupMemberIds: getCurrentAgentGroupMemberIds,
-  currentAgentGroupProfiles: getCurrentAgentGroupProfiles,
-  currentAgentGroupSupplierProfiles: getCurrentAgentGroupSupplierProfiles,
-  currentAgentGroupMemberProfiles: getCurrentAgentGroupMemberProfiles
+  currentAgent: getCurrentAgent
 })
