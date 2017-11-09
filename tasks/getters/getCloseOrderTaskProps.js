ec23b730d6bc4b3780247526b72d36c72b161f51
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-09 17:31:45 +1300

    slightly better close order task component / data

--- tasks/getters/getCloseOrderTaskProps.js
+++ tasks/getters/getCloseOrderTaskProps.js
@@ -1,6 +1,8 @@
 import { createStructuredSelector } from 'reselect'
 import getCurrentAgent from 'dogstack-agents/agents/getters/getCurrentAgent'
+import getCurrentOrder from '../../ordering/getters/getCurrentOrder'
 
 export default createStructuredSelector({
-  currentAgent: getCurrentAgent
+  currentAgent: getCurrentAgent,
+  currentOrder: getCurrentOrder
 })
