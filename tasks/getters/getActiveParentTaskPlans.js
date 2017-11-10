4d0ef8facf3e839d11b2e01b838691ca41b02d76
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-10 18:04:38 +1300

    use existing activeTaskPlans getter

--- tasks/getters/getActiveParentTaskPlans.js
+++ tasks/getters/getActiveParentTaskPlans.js
@@ -1,17 +1,14 @@
 import { createSelector } from 'reselect'
 import { prop, filter, pipe, isEmpty, isNil } from 'ramda'
 
-import getTaskPlans from './getTaskPlans'
+import getActiveTaskPlans from './getActiveTaskPlans'
 
 const getActiveParentTaskPlans = createSelector(
-  getTaskPlans,
-  pipe(
-    filter(pipe(prop('taskWork'), isEmpty)),
-    filter(pipe(
-      prop('parentTaskPlanId'),
-      isNil
-    ))
-  )
+  getActiveTaskPlans,
+  filter(pipe(
+    prop('parentTaskPlanId'),
+    isNil
+  ))
 )
 
 export default getActiveParentTaskPlans
