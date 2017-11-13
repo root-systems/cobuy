96e711dba29d8a4d47314d502ec2d29d1be0df40
Michael Smith <michael@msmith.nz>
2017-11-13 14:03:11 +0000

    add orders and intents db seed
    Will need to be removed once feature has been tested.

---
+++ db/seeds/3-orders-and-intents.js
@@ -0,0 +1,51 @@
+exports.seed = function (knex, Promise) {
+  const order = {
+    consumerAgentId: 2,
+    supplierAgentId: 3,
+    adminAgentId: 8
+  }
+
+  return knex('orders').insert(order).returning('id')
+    .then(([orderId]) => {
+      console.log(orderId)
+      const orderIntents = [{
+        agentId: 8,
+        desiredQuantity: 11,
+        productId: 1,
+        priceSpecId: 1,
+        orderId
+      },
+      {
+        agentId: 7,
+        desiredQuantity: 23,
+        productId: 1,
+        priceSpecId: 2,
+        orderId
+      },
+      {
+        agentId: 6,
+        desiredQuantity: 84,
+        productId: 1,
+        priceSpecId: 2,
+        orderId
+      },
+      {
+        agentId: 6,
+        desiredQuantity: 64,
+        productId: 1,
+        priceSpecId: 1,
+        orderId
+      }]
+
+      return Promise.all(orderIntents.map((intent) => knex('orderIntents').insert(intent)))
+      .then(() => {
+        return knex('taskPlans').insert({
+          taskRecipeId: 'closeOrder',
+          params: JSON.stringify({
+            orderId: 1
+          }),
+          assigneeId: 8
+        })
+      })
+    })
+}
