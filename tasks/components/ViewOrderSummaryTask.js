b9082d6f381297cd02c729c72b6e3d8cc4bd49d8
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-16 22:32:23 +1300

    WIP: towards querying for priceSpecs in ViewOrderSummaryTask container correctly

--- tasks/components/ViewOrderSummaryTask.js
+++ tasks/components/ViewOrderSummaryTask.js
@@ -1,10 +1,14 @@
 import h from 'react-hyperscript'
 import { isEmpty } from 'ramda'
+import { isNil, prop, any, pipe, values, tap, flatten, map } from 'ramda'
 
 import OrderSummary from '../../ordering/components/OrderSummary'
 
+import anyOrderPlansMissingPriceSpecs from '../util/anyOrderPlansMissingPriceSpecs'
+
 export default (props) => {
   const { currentOrderOrderPlansByAgent } = props
-  if (isEmpty(currentOrderOrderPlansByAgent)) return false
+  if (isEmpty(currentOrderOrderPlansByAgent)) return null
+  if (anyOrderPlansMissingPriceSpecs(currentOrderOrderPlansByAgent)) return null
   return h(OrderSummary, props)
 }
