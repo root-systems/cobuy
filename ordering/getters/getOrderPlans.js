26758f9eb89903f6088b54ae320454ac377e542a
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-18 18:39:15 +1300

    query for relevant products and resourceTypes for orderPlans of orderSummary

--- ordering/getters/getOrderPlans.js
+++ ordering/getters/getOrderPlans.js
@@ -3,15 +3,18 @@ import { values, pipe, map, filter, groupBy, prop, merge } from 'ramda'
 
 import getRawOrderPlans from './getRawOrderPlans'
 import getPriceSpecs from '../../supply/getters/getPriceSpecs'
+import getProducts from '../../supply/getters/getProducts'
 import { getAgents } from 'dogstack-agents/getters'
 
 export default createSelector(
   getRawOrderPlans,
   getPriceSpecs,
+  getProducts,
   getAgents,
-  (orderPlans, priceSpecs, agents) => {
+  (orderPlans, priceSpecs, products, agents) => {
     return map((orderPlan) => {
-      return merge(orderPlan, { priceSpec: priceSpecs[orderPlan.priceSpecId], agent: agents[orderPlan.agentId] })
+      const { priceSpecId, agentId, productId } = orderPlan
+      return merge(orderPlan, { priceSpec: priceSpecs[priceSpecId], agent: agents[agentId], product: products[productId] })
     }, orderPlans)
   }
 )
--- tasks/components/ViewOrderSummaryTask.js
+++ tasks/components/ViewOrderSummaryTask.js
@@ -6,13 +6,17 @@ import OrderSummary from '../../ordering/components/OrderSummary'
 
 import anyOrderPlansMissingPriceSpecs from '../util/anyOrderPlansMissingPriceSpecs'
 import anyOrderPlansMissingAgents from '../util/anyOrderPlansMissingAgents'
+import anyOrderPlansMissingProducts from '../util/anyOrderPlansMissingProducts'
 import anyOrderPlansMissingAgentProfiles from '../util/anyOrderPlansMissingAgentProfiles'
+import anyOrderPlansMissingProductResourceTypes from '../util/anyOrderPlansMissingProductResourceTypes'
 
 export default (props) => {
   const { currentOrderOrderPlansByAgent } = props
   if (isEmpty(currentOrderOrderPlansByAgent)) return null
   if (anyOrderPlansMissingPriceSpecs(currentOrderOrderPlansByAgent)) return null
   if (anyOrderPlansMissingAgents(currentOrderOrderPlansByAgent)) return null
+  if (anyOrderPlansMissingProducts(currentOrderOrderPlansByAgent)) return null
   if (anyOrderPlansMissingAgentProfiles(currentOrderOrderPlansByAgent)) return null
+  if (anyOrderPlansMissingProductResourceTypes(currentOrderOrderPlansByAgent)) return null
   return h(OrderSummary, props)
 }
