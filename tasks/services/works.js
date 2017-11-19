d4028e5c4c74ff4a6e643dd812446d695e036ed1
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-11-19 16:16:05 +1300

    fix assigning cast intent task to all group memebers when order closes

--- tasks/services/works.js
+++ tasks/services/works.js
@@ -31,31 +31,34 @@ function completeOrderSetupTaskWork (hook) {
 
 function createCastOrderIntentTaskPlan (hook) {
   const taskPlans = hook.app.service('taskPlans')
+  const orders = hook.app.service('orders')
   const relationships = hook.app.service('relationships')
   const taskRecipeId = taskRecipes.castIntent.id
   const completeOrderTaskPlanId = hook.data.taskPlanId
+  let params
 
   return taskPlans.get(completeOrderTaskPlanId)
   .then((completeOrderTaskPlan) => {
-    const groupId = completeOrderTaskPlan.params.consumerAgentId
     const orderId = completeOrderTaskPlan.params.orderId
-    const supplierAgentId = completeOrderTaskPlan.params.supplierAgentId
-    const params = { orderId, consumerAgentId: groupId, supplierAgentId }
-
+    return orders.get(orderId)
+  })
+  .then((order) => {
+    const { id, consumerAgentId, supplierAgentId, adminAgentId } = order
+    params = { orderId: id, consumerAgentId, supplierAgentId, adminAgentId }
     return relationships.find({
       query: {
-        sourceId: groupId,
+        sourceId: consumerAgentId,
         relationshipType: 'member'
       }
     })
-    .then((relationships) => {
-      return Promise.all(
-        map((relationship) => {
-          const assigneeId = prop('targetId', relationship)
-          return taskPlans.create({ taskRecipeId, params, assigneeId })
-        }, relationships)
-      )
-    })
+  })
+  .then((relationships) => {
+    return Promise.all(
+      map((relationship) => {
+        const assigneeId = prop('targetId', relationship)
+        return taskPlans.create({ taskRecipeId, params, assigneeId })
+      }, relationships)
+    )
   })
   .then(() => hook)
 }
