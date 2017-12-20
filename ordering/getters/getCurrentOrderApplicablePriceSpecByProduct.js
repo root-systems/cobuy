a9cedc3a15953fca672972f9d122d7b9162a994c
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-12-20 21:31:48 +1100

    fix for app crash when collective intents quantity is less than all minimums

--- ordering/getters/getCurrentOrderApplicablePriceSpecByProduct.js
+++ ordering/getters/getCurrentOrderApplicablePriceSpecByProduct.js
@@ -10,16 +10,22 @@ export default createSelector(
   (priceSpecsByProduct, collectiveQuantityByProductPrice) => {
     return mapObjIndexed((priceSpecs, productId) => {
       const quantityByPrice = collectiveQuantityByProductPrice[productId]
-
       // if no quantityByPrice, return the priceSpec with the smallest minimum for that product
       if (isNil(quantityByPrice)) {
         return priceSpecs[priceSpecs.length - 1]
       }
 
-      return find((priceSpec) => {
+      const applicablePriceSpec = find((priceSpec) => {
         const { id, minimum } = priceSpec
         return quantityByPrice[id] >= minimum
       }, priceSpecs)
+
+      // if there are intents, but no minimum has been reached yet, return the priceSpec with the smallest minimum for that product
+      if (isNil(applicablePriceSpec)) {
+        return priceSpecs[priceSpecs.length - 1]
+      }
+
+      return applicablePriceSpec
     }, priceSpecsByProduct)
   }
 )
