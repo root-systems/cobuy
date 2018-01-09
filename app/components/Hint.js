47b1abfa6f18f12235b487381ad9bb805802cc77
Michael Williams <michael.williams@enspiral.com>
2018-01-10 11:43:35 +1300

    better Hint component, prop types, customizable icon, default icon

--- app/components/Hint.js
+++ app/components/Hint.js
@@ -1,4 +1,5 @@
 import h from 'react-hyperscript'
+import PropTypes from 'prop-types'
 import { compose } from 'recompose'
 import { connect as connectStyles } from 'react-fela'
 import IconButton from 'material-ui/IconButton'
@@ -11,18 +12,27 @@ export default compose(
 )(Hint)
 
 function Hint (props) {
-  const { text } = props
+  const { text, icon } = props
 
   return (
     h(IconButton, {
       tooltip: text
     }, [
       h(FontIcon, {
-        className: 'fa fa-info-circle'
-        // className: 'fa fa-question-circle'
-        // className: 'fa fa-info'
-        // className: 'fa fa-question'
+        className: `fa fa-${icon}-circle`
       })
     ])
   )
+}
+
+Hint.propTypes = {
+  text: PropTypes.isRequired,
+  icon: PropTypes.oneOf([
+    'info',
+    'question'
+  ])
+}
+
+Hint.defaultProps = {
+  icon: 'info'
 }
