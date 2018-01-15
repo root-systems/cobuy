5ddf6def8a82a20cd8ac51863fc08bce716d5113
Juliet <to.julietc@gmail.com>
2018-01-15 17:59:18 +1300

    updated email layout to look less ugly

--- app/mjml/welcome.js
+++ app/mjml/welcome.js
@@ -1,24 +1,41 @@
 import { mjml2html } from 'mjml'
 
-export default function welcome ({ app, token }) {
+import style from '../../style'
+
+export default function welcome ({ app, token, assetsUrl }) {
+  console.log(`${assetsUrl}/images/tapinBuy.png`)
+  console.log(`${style.theme.colors.primary1}`)
   return mjml2html(`
     <mjml>
       <mj-body>
         <mj-container>
+
+        <mj-section>
+          <mj-column>
+            <mj-image src="${assetsUrl}/images/tapinBuy.png">
+            </mj-image>
+          </mj-column>
+        </mj-section>
+
           <mj-section>
             <mj-column>
-              <mj-text>
-                Hi. You're invited to join a group on ${app.name}!
+              <mj-text font-size="30">
+                <p>Hi.</p>
+                <p>You're invited to join a group on ${app.name}!</p>
               </mj-text>
             </mj-column>
           </mj-section>
+
           <mj-section>
             <mj-column>
-              <mj-text>
-                ${app.bodyText}
+              <mj-text font-size="18">
+              <p>Cobuy is an app that makes buying groups easy to start, maintain, and grow.</p>
+              <p> Using the collective buying power of a group, we can buy food in bulk directly from wholesalers.</p>
+              <p>By cutting out retailers, we effectively eliminate retail food waste, save money and have access to a wider range of better quality products.</p>
               </mj-text>
             </mj-column>
           </mj-section>
+
           <mj-section>
             <mj-column>
               <mj-text>
@@ -26,6 +43,9 @@ export default function welcome ({ app, token }) {
               </mj-text>
             </mj-column>
           </mj-section>
+
+          <mj-section background-color=${style.theme.colors.primary1}>asdasdasdasdasd</mj-section>
+
         </mj-container>
       </mj-body>
     </mjml>
