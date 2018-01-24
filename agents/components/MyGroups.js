9750010cf4f96f2084b48230377f404ee348f228
Gregory Kan <kan.gregory@gmail.com>
2018-01-25 11:32:42 +1300

    feat(profile): fixed style refs

--- agents/components/MyGroups.js
+++ agents/components/MyGroups.js
@@ -1,12 +1,14 @@
 import h from 'react-hyperscript'
+import { connect as connectFela } from 'react-fela'
 import { isNil, isEmpty, map } from 'ramda'
 import { FormattedMessage } from 'dogstack/intl'
+import { compose } from 'recompose'
 
 // import Profile from '../../agents/components/Profile'
 import styles from '../styles/MyGroups'
 
-export default (props) => {
-  const { actions, currentAgent, currentAgentGroupProfiles } = props
+function MyGroups (props) {
+  const { styles, currentAgent, currentAgentGroupProfiles } = props
 
   if (isNil(currentAgent)) {
     return null
@@ -41,3 +43,7 @@ export default (props) => {
     renderMyGroups()
   ])
 }
+
+export default compose(
+  connectFela(styles)
+)(MyGroups)
