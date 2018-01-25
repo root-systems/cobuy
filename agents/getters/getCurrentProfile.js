32e1955aa3bdaba38ab38fb72e72034766bf71e6
Gregory Kan <kan.gregory@gmail.com>
2018-01-25 17:43:30 +1300

    feat(profiles): new gettings for getting profiles state and current profile

--- agents/getters/getCurrentProfile.js
+++ agents/getters/getCurrentProfile.js
@@ -1,11 +1,11 @@
 import { createSelector } from 'reselect'
 import { prop } from 'ramda'
 
-import getCurrentAgentAllProfiles from './getCurrentAgentAllProfiles'
+import getProfiles from './getProfiles'
 import getCurrentProfileId from './getCurrentProfileId'
 
 export default createSelector(
   getCurrentProfileId,
-  getCurrentAgentAllProfiles,
+  getProfiles,
   prop
 )
--- agents/getters/getProfileProps.js
+++ agents/getters/getProfileProps.js
@@ -1,11 +1,9 @@
 import { createStructuredSelector } from 'reselect'
-import getCurrentAgent from 'dogstack-agents/agents/getters/getCurrentAgent'
 
 import getCurrentProfile from './getCurrentProfile'
 
-const getOrderPageProps = createStructuredSelector({
-  currentAgent: getCurrentAgent,
+const getProfileProps = createStructuredSelector({
   profile: getCurrentProfile
 })
 
-export default getOrderPageProps
+export default getProfileProps
