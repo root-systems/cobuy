f53b0722a301e31e60030b7745e7dffa4c699e1b
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2018-01-30 15:08:47 +1300

    resourceTypes hooks, unfinished tests

--- ordering/services/orders.js
+++ ordering/services/orders.js
@@ -7,6 +7,8 @@ import { pipe, equals, length, isNil, isEmpty, map, any, find, not, prop } from 
 import * as taskRecipes from '../../tasks/data/recipes'
 
 import restrictToCurrentUserGroups from '../../lib/hooks/restrictToCurrentUserGroups'
+import restrictToGroupAdmin from '../../lib/hooks/restrictToGroupAdmin'
+import restrictToOrderOrGroupAdmin from '../../lib/hooks/restrictToOrderOrGroupAdmin'
 
 module.exports = function () {
   const app = this
@@ -43,56 +45,6 @@ const hooks = {
       iffElse(hasNotCompletedGroupOrSupplierProfile, createCompleteOrderSetupWithPreReqsTaskPlan, createCompleteOrderSetupTaskPlan)
     ]
   }
-}
-
-function restrictToGroupAdmin (hook) {
-  // If it was an internal call then skip this hook
-  if (!hook.params.provider) {
-    return hook;
-  }
-  const { agentId } = hook.params.credential
-  const { consumerAgentId } = hook.data
-  return hook.app.service('relationships').find({
-    query: {
-      sourceId: consumerAgentId,
-      targetId: agentId,
-      relationshipType: 'admin'
-    }
-  })
-  .then((relationships) => {
-    if (isEmpty(relationships)) {
-      throw new errors.Forbidden('You must be the group admin to create an order.')
-    }
-    return hook
-  })
-}
-
-function restrictToOrderOrGroupAdmin (hook) {
-  // If it was an internal call then skip this hook
-  if (!hook.params.provider) {
-    return hook;
-  }
-  const { agentId } = hook.params.credential
-  const { id } = hook
-  return hook.app.service('orders').get(id, hook.params)
-  .then((order) => {
-    const { adminAgentId, consumerAgentId } = order
-    if (agentId === adminAgentId) return hook
-
-    return hook.app.service('relationships').find({
-      query: {
-        sourceId: consumerAgentId,
-        targetId: agentId,
-        relationshipType: 'admin'
-      }
-    })
-    .then((relationships) => {
-      if (isEmpty(relationships)) {
-        throw new errors.Forbidden('You must be the group admin to create an order.')
-      }
-      return hook
-    })
-  })
 }
 
 function getCurrentUser (hook) {
--- resources/services/resourceTypes.js
+++ resources/services/resourceTypes.js
@@ -1,4 +1,8 @@
 const feathersKnex = require('feathers-knex')
+import errors from 'feathers-errors'
+import { hooks as authHooks } from 'feathers-authentication'
+const { authenticate } = authHooks
+import { prop, merge, difference, without, contains, isEmpty } from 'ramda'
 
 module.exports = function () {
   const app = this
@@ -11,8 +15,108 @@ module.exports = function () {
   app.service(name).hooks(hooks)
 }
 
+// TODO: IK: not clear to me what the ideal protection is for CUPR... when a resource is created it doesn't necessarily need an existing product, so at the moment it's just restricted to any group admins (not ideal)
 const hooks = {
-  before: {},
+  before: {
+    // all: authenticate('jwt'),
+    find: restrictToCurrentUserGroupsSuppliersProducts,
+    get: restrictToCurrentUserGroupsSuppliersProducts,
+    create: restrictToAnyGroupAdmin,
+    update: restrictToAnyGroupAdmin,
+    patch: restrictToAnyGroupAdmin,
+    remove: restrictToAnyGroupAdmin
+  },
   after: {},
   error: {}
+}
+
+function restrictToCurrentUserGroupsSuppliersProducts (hook) {
+  // use $in syntax to restrict arrays of results to certain values
+  // by adding conditions to the query params
+  const { agentId } = hook.params.credential
+  return hook.app.service('relationships').find({
+    query: {
+      targetId: agentId,
+      relationshipType: 'member'
+    }
+  })
+  .then((groups) => {
+    const groupIds = groups.map(prop('sourceId'))
+    return hook.app.service('relationships').find({
+      query: {
+        sourceId: {
+          $in: groupIds
+        },
+        relationshipType: 'supplier'
+      }
+    })
+  })
+  .then((suppliers) => {
+    const supplierIds = suppliers.map(prop('targetId'))
+    return hook.app.service('products').find({
+      query: {
+        supplierAgentId: {
+          $in: supplierIds
+        }
+      }
+    })
+  })
+  .then((products) => {
+    // TODO: IK: clean this mess up!
+    const resourceTypeIds = products.map(prop('resourceTypeId'))
+    if (!hook.params.query) {
+      hook.params.query = {
+        id: {
+          $in: resourceTypeIds
+        }
+      }
+      return hook
+    }
+    if (!hook.params.query.id) {
+      hook.params.query = merge(hook.params.query, {
+        id: {
+          $in: resourceTypeIds
+        }
+      })
+      return hook
+    }
+    // either a number, or an object with querying syntax
+    if (typeof hook.params.query.id === 'object') {
+      if (hook.params.query.id.$in) {
+        const disallowedResourceTypes = difference(hook.params.query.id.$in, resourceTypeIds)
+        hook.params.query.id.$in = without(disallowedResourceTypes, hook.params.query.id.$in)
+        return hook
+      }
+      hook.params.query.id = merge(hook.params.query.id, { $in: resourceTypeIds })
+      return hook
+    }
+    if (!contains(hook.params.query.id, resourceTypeIds)) {
+      hook.params.query.id = -1 // TODO: IK: is there a better way to achieve this outcome?
+      return hook
+    }
+    if (contains(hook.params.query.id, resourceTypeIds)) {
+      return hook
+    }
+    throw new Error(`couldn't restrict ${hook.service} query correctly for method ${hook.method}`)
+  })
+}
+
+function restrictToAnyGroupAdmin (hook) {
+  // If it was an internal call then skip this hook
+  if (!hook.params.provider) {
+    return hook;
+  }
+  const { agentId } = hook.params.credential
+  return hook.app.service('relationships').find({
+    query: {
+      targetId: agentId,
+      relationshipType: 'admin'
+    }
+  })
+  .then((relationships) => {
+    if (isEmpty(relationships)) {
+      throw new errors.Forbidden(`You must be an admin of a group to execute ${hook.method} on ${hook.service}.`)
+    }
+    return hook
+  })
 }
