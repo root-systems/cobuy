fbde95c787b5f9166b936fd7271cdce246672214
Gregory Kan <kan.gregory@gmail.com>
2018-02-01 20:29:14 +1300

    chore(refactor): removed leftovers

--- tasks/getters/getSetupGroupTaskProps.js
+++ tasks/getters/getSetupGroupTaskProps.js
@@ -1,6 +1,5 @@
 import { createStructuredSelector } from 'reselect'
 
-// import getRelationshipsByMemberAgentId from '../../agents/getters/getRelationshipsByMemberAgentId'
 import getRelationships from '../../agents/getters/getRelationships'
 
 export default createStructuredSelector({
