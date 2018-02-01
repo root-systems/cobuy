7a22c587993e873cf70c66a0b553cd5d440c32b5
Gregory Kan <kan.gregory@gmail.com>
2018-02-01 20:04:37 +1300

    feat(members): added guard for not-yet-persisted member

--- tasks/components/SetupGroupTask.js
+++ tasks/components/SetupGroupTask.js
@@ -47,6 +47,7 @@ export default (props) => {
           members
         },
         removeMember: (memberVal) => {
+          if (isNil(memberVal.agentId)) return
           const memberRelationship = memberRelationships[memberVal.agentId]
           actions.relationships.remove(memberRelationship.id)
         },
